d9218ddf265ba864f233f7e0c87c6f466604a76d
Aarya Rastogi <97822457+aaryarastogi@users.noreply.github.com>
2025-04-24 16:44:32 +0530

    final done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import './App.css';
-import VideoPlayer from './Components/VideoPlayer';
 import VideosList from './Components/VideosList';
 
 function App() {
-  const videoId = "video-001";
   return (
     <div className="App">
       <VideosList/>
